33d3172f1524954ebb89fe94868b69febfc5537c
Gautam Makwana <makwanagautam411@gmail.com>
2026-04-13 05:41:44 +0530

    allow all origins

--- src/app.ts
+++ src/app.ts
@@ -35,9 +35,10 @@ app.use(cookieParser());
 
 // CORS
 app.use(cors({
-    origin: [...config.cors.clientUrl, 'http://localhost:8080'],
+    origin: true,
     credentials: true
 }));
+
 
 
 // Basic route
